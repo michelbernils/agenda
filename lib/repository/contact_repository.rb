f66296701bd0fa109838d35476af5e1329e120c3
Michel Bernils <michelbernils@MacBook-Pro-de-Michel.local>
2022-05-24 19:43:40 -0300

    robocup changes

--- lib/repository/contact_repository.rb
+++ lib/repository/contact_repository.rb
@@ -1,59 +1,68 @@
+# frozen_string_literal: true
+
 require_relative '../../lib/entity/contact'
+
 require 'csv'
 load('../lib/agenda.thor')
 
-Class ContactRepository
-
+# This class contains all the logic necessary to run the agenda
+class ContactRepository
   def start_agenda(file_name)
     agenda = Agenda.new(file_name: file_name)
     file_path = "../files/#{agenda.file_name}"
-    headers = ['name','email']
+    headers = %w[name header]
     CSV.open(file_path, 'a+') do |csv|
-      csv << headers if csv.count.eql? 0 
+      csv << headers if csv.count.eql? 0
       end
-    end
   end
 
-  def file_exists?(file_name)
-    if(File.exist?("../files/#{file_name}")) 
-      true
-    else 
-      false
+  def add_contact_to_csv(file_name, name, email)
+    return unless File.exist?("../files/#{file_name}")
+
+    CSV.open("../files/#{file_name}", 'a+') do |csv|
+      csv << [contact.name, contact.email]
     end
   end
 
   def add_contact_to_csv(file_name, name, email)
     return unless File.exist?("../files/#{file_name}")
-    CSV.open("../files/#{file_name}", "a+") do |csv|
+
+    CSV.open("../files/#{file_name}", 'a+') do |csv|
       csv << [contact.name, contact.email]
-      end
     end
   end
 
   def search_contact_on_csv_using_name(file_name, name)
     return unless File.exist?("../files/#{file_name}")
+
     csv = CSV.parse(File.read("../files/#{file_name}"), headers: true)
-      if (csv.find {|row| row["name"] == contact.name})
-        puts "Usuario encontrado"
-        puts contact.name
-      else
-        puts "User not found"
-      end
+    if csv.find { |row| row['name'] == contact.name }
+      puts 'Usuario encontrado'
+      puts contact.name
+    else
+      puts 'User not found'
     end
   end
 
   def delete_contact_on_csv_using_name(file_name, name)
     return unless File.exist?("../files/#{file_name}")
-    table = CSV.table(("../files/#{file_name}")
-      table.delete_if do |row|
-        row[:name] == contact.name
-      end
-    
-      File.open("../files/#{file_name}", 'w') do |f|
-        f.write(table.to_csv)
-      end
+
+    table = CSV.table("../files/#{file_name}")
+    table.delete_if do |row|
+      row[:name] == contact.name
+    end
+    File.open("../files/#{file_name}", 'w') do |f|
+      f.write(table.to_csv)
     end
   end
-  
-end
 
+  private
+
+  def file_exists?(file_name)
+    if File.exist?("../files/#{file_name}")
+      true
+    else
+      false
+    end
+  end
+end
